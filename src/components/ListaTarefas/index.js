b2e74741e1231798166a397a6b18c01cdfae8c17
FeZanette <fernandazanette1975@gmail.com>
2023-09-04 14:44:36 -0300

    Renderização de listas aula template finalizada

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -11,16 +11,37 @@ import {
 import bin from "../../assets/bin.png";
 
 export function ListaTarefas() {
-  const [novaTarefa, setNovaTarefa] = useState("");
+  const listaDeTarefas = ["comer", "dormir", "estudar"];
 
+  const [novaTarefa, setNovaTarefa] = useState("");
+  const [lista, setLista] = useState(listaDeTarefas);
+
+  //  FUNÇÃO MAP CHAMADA FORA DO JSX:
+  // const listaMapeada = lista.map((item) => {
+  //   return (
+  //     <Tarefa>
+  //       <p>{item}</p>
+  //       <RemoveButton>
+  //         <img src={bin} alt="" width="16px" />
+  //       </RemoveButton>
+  //     </Tarefa>
+  //   );
+  // });
 
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
 
-  const adicionaTarefa = () => {};
+  const adicionaTarefa = () => {
+    const novaLista = [...lista, novaTarefa];
+    setLista(novaLista);
+    setNovaTarefa("");
+  };
 
-  const removeTarefa = () => {};
+  const removeTarefa = (valor) => {
+    const filtrarItem = lista.filter((item) => item !== valor);
+    setLista(filtrarItem);
+  };
 
   return (
     <ListaTarefasContainer>
@@ -30,17 +51,25 @@ export function ListaTarefas() {
           value={novaTarefa}
           onChange={onChangeTarefa}
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
 
       <ListaContainer>
         <ul>
-          <Tarefa>
-            <p>Nova tarefa</p>
-            <RemoveButton>
-              <img src={bin} alt="" width="16px" />
-            </RemoveButton>
-          </Tarefa>
+          {/* FUNÇÃO MAP CHAMADA FORA DO JSX: */}
+          {/* {listaMapeada} */}
+
+          {/* FUNÇÃO MAP CHAMADA DENTRO DO JSX: */}
+          {lista.map((item) => {
+            return (
+              <Tarefa>
+                <p>{item}</p>
+                <RemoveButton onClick={() => removeTarefa(item)}>
+                  <img src={bin} alt="" width="16px" />
+                </RemoveButton>
+              </Tarefa>
+            );
+          })}
         </ul>
       </ListaContainer>
     </ListaTarefasContainer>
